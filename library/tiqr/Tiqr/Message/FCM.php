4a5fb7cea54c0c6ae069f05e4d164c2912b3c4db
Peter Havekes <peter.havekes@surfnet.nl>
2020-10-23 10:51:44 +0200

    Wait and retry once in case of a 502 Bad Gateway error

--- library/tiqr/Tiqr/Message/FCM.php
+++ library/tiqr/Tiqr/Message/FCM.php
@@ -1,15 +1,15 @@
 <?php
 /**
  * This file is part of the tiqr project.
- * 
- * The tiqr project aims to provide an open implementation for 
- * authentication using mobile devices. It was initiated by 
+ *
+ * The tiqr project aims to provide an open implementation for
+ * authentication using mobile devices. It was initiated by
  * SURFnet and developed by Egeniq.
  *
  * More information: http://www.tiqr.org
  *
  * @author Joost van Dijk <joost.vandijk@surfnet.nl>
- * 
+ *
  * @package tiqr
  *
  * @license New BSD License - See LICENSE file for details.
@@ -52,11 +52,12 @@ class Tiqr_Message_FCM extends Tiqr_Message_Abstract
      * @param $alert string alert message
      * @param $challenge string tiqr challenge url
      * @param $apiKey string api key for firebase
+     * @param $retry boolean is this a 2nd attempt
      * @param Tiqr_Message_Exception $gcmException
      *
      * @throws Tiqr_Message_Exception_SendFailure
      */
-    private function _sendFirebase($deviceToken, $alert, $challenge, $apiKey)
+    private function _sendFirebase($deviceToken, $alert, $challenge, $apiKey, $retry=false)
     {
         $msg = array(
             'challenge' => $challenge,
@@ -90,6 +91,13 @@ class Tiqr_Message_FCM extends Tiqr_Message_Abstract
 
         if (!empty($errors)) {
             throw new Tiqr_Message_Exception_SendFailure("Http error occurred: ". $errors, true);
+        }
+
+        // Wait and retry once in case of a 502 Bad Gateway error
+        if (($statusCode == 502) && !($retry)) {
+          sleep(2);
+          $this->_sendFirebase($deviceToken, $alert, $challenge, $apiKey, true);
+          return;
         }
 
         if ($statusCode !== 200) {
