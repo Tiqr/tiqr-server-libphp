ad3f5f1d4e3cb9be9067e4e7222dd7086b46a770
Peter Havekes <peter@havekes.eu>
2024-01-29 13:38:13 +0100

    Name the testserver-docker instance

--- run-docker-testserver.sh
+++ run-docker-testserver.sh
@@ -8,4 +8,4 @@ CONFIG_DIR="${SCRIPT_DIR}/TestServer/config/"
 echo CONFIG_DIR="${CONFIG_DIR}"
 STORAGE_DIR="${SCRIPT_DIR}/TestServer/storage/"
 echo STORAGE_DIR="${STORAGE_DIR}"
-docker run -it --rm -v "${CONFIG_DIR}:/var/www/TestServer/config" -v "${STORAGE_DIR}:/var/www/TestServer/storage" -p 8000:80/tcp tiqr-testserver:latest
+docker run -it --rm -v "${CONFIG_DIR}:/var/www/TestServer/config" -v "${STORAGE_DIR}:/var/www/TestServer/storage" --name tiqr-testserver -p 8000:80/tcp tiqr-testserver:latest
